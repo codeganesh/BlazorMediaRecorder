7d3f3ef1376fea586d5bfbc4c5ccd4cf8cc72e9c
codeganesh <codeganesh@gmail.com>
2023-11-15 06:37:19 +0300

    Updated README.md

--- GkbApps.Blazor.MediaRecorder.WebUI/GkbApps.Blazor.MediaRecorder.WebUI.Client/Program.cs
+++
@@ -1,8 +0,0 @@
-using GkbApps.Blazor.MediaRecorder;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddMediaDevicesEnumerator();
-
-await builder.Build().RunAsync();
